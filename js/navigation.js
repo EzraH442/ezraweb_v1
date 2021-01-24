e987e00b8e7a40a5d3fd2394d99065901e933363
EzraH442 <ezrahuang155@gmail.com>
2021-01-24 14:18:52 -0700

    Improved navigation colors and spacing

--- js/navigation.js
+++ js/navigation.js
@@ -2,16 +2,23 @@ const toggle = document.getElementById("toggle");
 const menu = document.querySelector(".top-navigation");
 const submenuToggles = document.getElementsByClassName("toggle-submenu");
 
+function getDropdownMenus() {
+    return document.getElementsByClassName("submenu-active");
+}
+function closeDropdowns() {
+    let x = getDropdownMenus();
+    for (let i = 0; i < x.length; i++) {
+        x[i].classList.remove("submenu-active");
+    }
+}
 function toggleMenu() {
     if (menu.classList.contains("active")) {
         menu.classList.remove("active");
+        closeDropdowns();
     } else {
         menu.classList.add("active");
     }
 }
-
-toggle.addEventListener("click", toggleMenu, false);
-
 function toggleDropdown() {
     if (this.parentNode.parentNode.classList.contains("submenu-active")) {
         this.parentNode.parentNode.classList.remove("submenu-active");
@@ -19,7 +26,13 @@ function toggleDropdown() {
         this.parentNode.parentNode.classList.add("submenu-active");
     }
 }
-
+function handleResize() {
+    if (window.innerWidth > 960) {
+        closeDropdowns();
+    }
+}
+toggle.addEventListener("click", toggleMenu, false);
 for (let i = 0; i < submenuToggles.length; i++) {
     submenuToggles[i].addEventListener('click', toggleDropdown, false);
 }
+window.addEventListener('resize', handleResize, false);
