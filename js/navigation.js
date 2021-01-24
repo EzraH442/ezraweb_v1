948bc65e91d77f87246e99022df2675d339646c4
EzraH442 <ezrahuang155@gmail.com>
2021-01-24 09:31:12 -0700

    revamped naviagtion with dropdown icons

--- js/navigation.js
+++ js/navigation.js
@@ -1,5 +1,6 @@
-const toggle = document.querySelector(".toggle");
+const toggle = document.getElementById("toggle");
 const menu = document.querySelector(".top-navigation");
+const submenuToggles = document.getElementsByClassName("toggle-submenu");
 
 function toggleMenu() {
     if (menu.classList.contains("active")) {
@@ -10,3 +11,15 @@ function toggleMenu() {
 }
 
 toggle.addEventListener("click", toggleMenu, false);
+
+function toggleDropdown() {
+    if (this.parentNode.parentNode.classList.contains("submenu-active")) {
+        this.parentNode.parentNode.classList.remove("submenu-active");
+    } else {
+        this.parentNode.parentNode.classList.add("submenu-active");
+    }
+}
+
+for (let i = 0; i < submenuToggles.length; i++) {
+    submenuToggles[i].addEventListener('click', toggleDropdown, false);
+}
